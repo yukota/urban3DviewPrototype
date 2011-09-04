88d6e4d176136046967d5c31ab4c0023b7c8a3d5
YuK_Ota <whitenightwinter@gmail.com>
2011-09-04 23:11:47 +0900

    OPENGLが使用できないのでP3Dを使用するよう変更

--- src/com/github/yukota/urban3dviewprototype/Main.java
+++ src/com/github/yukota/urban3dviewprototype/Main.java
@@ -11,6 +11,10 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        
+        Runtime.getRuntime().addShutdownHook(new Shutdown());
+        
+        
     	UrbanController urbanController = new UrbanController();
     	//画面サイズHeight
     	int initHeight = 480;
@@ -21,4 +25,21 @@ public class Main {
 
     }
 
+}
+
+/**
+ * 終了時処理.
+ * @author YuK_Ota
+ *
+ */
+class Shutdown extends Thread {
+    public Shutdown(){
+       super();
+    }
+    
+    @Override
+    public void run() {
+       System.out.println("end process");
+    }
+    
 }
--- src/com/github/yukota/urban3dviewprototype/MapPApplet.java
+++ src/com/github/yukota/urban3dviewprototype/MapPApplet.java
@@ -1,11 +1,14 @@
 package com.github.yukota.urban3dviewprototype;
 
 import processing.core.PApplet;
+import processing.opengl.*;
 
 public class MapPApplet extends PApplet {
     
     @Override
     public void setup() {
+        size(100, 100,P3D);
+        background(0, 80, 80);
     }
     
     /**
@@ -14,8 +17,6 @@ public class MapPApplet extends PApplet {
     @Override
     public void draw() {
         //背景色
-        
-        background(0, 80, 80);
         
     }
     
