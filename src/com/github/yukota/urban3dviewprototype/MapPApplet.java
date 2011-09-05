32342cdc8523c0a7a48793642d88452baf513534
YuK_Ota <whitenightwinter@gmail.com>
2011-09-06 01:13:40 +0900

    3dっぽいの表示

--- src/com/github/yukota/urban3dviewprototype/MapPApplet.java
+++ src/com/github/yukota/urban3dviewprototype/MapPApplet.java
@@ -6,8 +6,7 @@ public class MapPApplet extends PApplet {
     
     @Override
     public void setup() {
-        size(100, 100,P3D);
-        background(0, 80, 80);
+        size(640, 480,P3D);
     }
     
     /**
@@ -16,8 +15,73 @@ public class MapPApplet extends PApplet {
     @Override
     public void draw() {
         //背景色
+        background(0);
+        //default ambientlight,directional light
+        lights();
+        //中心地に平行移動
+        translate(320,240);
+        rotateX(10);
+        
+        Agent3D oneAgent = new Agent3D();
+        oneAgent.display();
+        
+        //人のモデルの作成
+        
+        
+        
+//        box(100);
+    }
+    
+ 
+public class Agent3D {
+
+    private float height = 200;
+    private float width= height*2/3;
+    
+    private float headSize = height/3;
+    private float bodyHeight = height * 2 / 3;
+    
+    private float bodyWidht = width ;
+    
+    private float frontWidth = width/2;
+    /**
+     * コンストラクタ
+     */
+    public Agent3D() {
         
     }
+    
+    
+    public void display() {
+    /*
+     * Draw Agent
+     */
+        
+    //頭の描写
+       // sphere(headSize);
+    //胴体の描写
+        fill(0,0,255);
+        beginShape();
+        
+        //frame1
+        vertex(0,0,0);
+        vertex(bodyWidht/2, 0, bodyHeight);
+        vertex(0, -frontWidth, bodyHeight);
+        vertex(-bodyWidht/2, 0, bodyHeight);
+        
+        
+        
+        endShape(CLOSE);
+        
+            
+        
+        
+    
+    //胴体の描写
+    
+    }
+}
+   
     
 
 }
